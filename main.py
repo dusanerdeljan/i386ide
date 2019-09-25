8748557a2c4164198fe4513a91e012c5924356f8
dusanerdeljan <dusanerdeljan99@gmail.com>
2019-09-25 16:26:03 +0200

    Promenjena putanja za ucitavanje resursa. Ova putanja se koristi kada se program pokrece iz terminala.

--- main.py
+++ main.py
@@ -422,9 +422,11 @@ def resource_path(relative_path):
     """ Get absolute path to resource, works for dev and for PyInstaller """
     try:
         # PyInstaller creates a temp folder and stores path in _MEIPASS
-        base_path = sys._MEIPASS
+        # base_path = sys._MEIPASS
+        base_path = PathManager.START_DIRECTORY
     except Exception:
-        base_path = os.path.abspath(".")
+        # base_path = os.path.abspath(".")
+        base_path = PathManager.START_DIRECTORY
 
     return os.path.join(base_path, relative_path)
 
