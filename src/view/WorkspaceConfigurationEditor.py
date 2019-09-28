e7b17b14b450bbead6446dd019e74b72b4b14f30
dusanerdeljan <dusanerdeljan99@gmail.com>
2019-09-28 10:21:55 +0200

    Promenjene ikonice.

--- src/view/WorkspaceConfigurationEditor.py
+++ src/view/WorkspaceConfigurationEditor.py
@@ -1,15 +1,19 @@
 """
-    This file is part of i386ide.
-    i386ide is free software: you can redistribute it and/or modify
+    i386ide is lightweight IDE for i386 assembly and C programming language.
+    Copyright (C) 2019  Dušan Erdeljan, Marko Njegomir
+
+    This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU General Public License as published by
     the Free Software Foundation, either version 3 of the License, or
     (at your option) any later version.
+
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
     GNU General Public License for more details.
+
     You should have received a copy of the GNU General Public License
-    along with this program.  If not, see <https://www.gnu.org/licenses/>.
+    along with this program.  If not, see <https://www.gnu.org/licenses/>
 """
 
 from PySide2.QtWidgets import QDialog, QPushButton, QVBoxLayout, QHBoxLayout, QComboBox, QCheckBox, QFileDialog, QMessageBox
