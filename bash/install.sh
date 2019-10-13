36b25f3473a2d6437563883be658625a24c64a26
dusanerdeljan <dusanerdeljan99@gmail.com>
2019-10-13 21:14:06 +0200

    Doradjen skript za instalaciju programa. Proverava se da li postoji izvrsni fajl, ikonica i da li je program vec instaliran. Nakon instalacije skript ce se obrisati.

--- bash/install.sh
+++ bash/install.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 EXE_PATH="${DIR}/i386ide"
+if [ ! -f "$EXE_PATH" ]; then
+	echo "Error: Executable ${EXE_PATH} does not exist!"
+	exit 1
+fi
 ICON_PATH="${DIR}/icon.png"
+if [ ! -f "$ICON_PATH" ]; then
+	echo "Error: Icon ${ICON_PATH} does not exist!"
+	exit 1
+fi
 DESKTOP_FILE_PATH="${DIR}/i386ide.desktop"
 USERNAME=$SUDO_USER
 DEST_PATH="/home/${USERNAME}/Desktop/.i386ide_application_data"
-mkdir $DEST_PATH
+if [ -d "$DEST_PATH" ]; then
+	echo "Error: ${DIR} already exists! Please uninstall the program first!"
+	exit 1
+else
+	mkdir $DEST_PATH
+fi
 DEST_EXE_PATH="${DEST_PATH}/i386ide"
 DEST_ICON_PATH="${DEST_PATH}/icon.png"
 sudo mv $EXE_PATH $DEST_EXE_PATH
@@ -23,4 +36,4 @@ Name=i386ide
 Icon=${DEST_ICON_PATH}" >> ./i386ide.desktop
 sudo mv $DESKTOP_FILE_PATH /usr/share/applications/
 sudo install -m 0755 ${DEST_EXE_PATH} /usr/local/bin
-
+sudo rm -f "${DIR}/install.sh"
